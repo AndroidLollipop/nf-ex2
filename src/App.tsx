4068dede9aba87f05461623271892580f5fe018c
AndroidLollipop <obfuscated00143241131643657478@gmail.com>
2022-01-22 13:57:24 +0800

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mockData from './mockData'
 
-function App() {
+enum Scene {
+  Mountain = "Mountain",
+  Beaches = "Beaches",
+  Birds = "Birds",
+  Food = "Food"
+}
+
+type tabData = {scene: Scene, name: string}
+type displayData = {name: string, pictures: Promise<string[]>}
+
+const tabs: tabData[] = [
+  {scene: Scene.Mountain, name: "Mountain"},
+  {scene: Scene.Beaches, name: "Beaches"},
+  {scene: Scene.Birds, name: "Birds"},
+  {scene: Scene.Food, name: "Food"}
+]
+
+const Tab = (setScene: Function) => ({scene, name}: tabData) => {
+  return (
+    <button key={scene} className="tab" onClick={() => setScene(scene)}>{name}</button>
+  )
+}
+
+const useLoader = (pictures: Promise<string[]>) => {
+  const [loading, setLoading] = React.useState(true)
+  const [pics, setPics] = React.useState([] as string[])
+  React.useEffect(() => {
+    setLoading(true)
+    let cancelled = false
+    pictures.then(picURLs => {
+      if (cancelled) return
+      setLoading(false)
+      setPics(picURLs)
+    })
+    return () => {
+      cancelled = true
+    }
+  }, [pictures])
+  return {loading, pics}
+}
+
+const Display = ({name, pictures}: displayData) => {
+  const {loading, pics} = useLoader(pictures)
+  return (
+    <div>
+      <div className="displayName">{name} Pictures</div>
+      {loading ? 
+        <div className="imageGridContainer">Loading...</div> : 
+        <div className="imageGridContainer">
+          {pics.map((pic, index) => 
+            <div className="imageContainer">
+              <img className="image" key={index} src={pic} alt={name}/>
+            </div>
+          )}
+        </div>
+      }
+    </div>
+  )
+}
+
+const App = () => {
+  const [scene, setScene] = React.useState(Scene.Mountain)
+  const TabMemo = React.useMemo(() => Tab(setScene), [setScene])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tabs">
+        {tabs.map(TabMemo)}
+      </div>
+      <Display name={scene} pictures={mockData[scene]}/>
     </div>
   );
 }
